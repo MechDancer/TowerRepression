02d70b8063982f49f388017b098d888c1919a28f
berberman <1793913507@qq.com>
2019-07-03 14:48:07 +0800

    add smart table

--- app/src/main/java/org/mechdancer/towerrepression/table/TableUI.kt
+++ app/src/main/java/org/mechdancer/towerrepression/table/TableUI.kt
@@ -2,14 +2,19 @@ package org.mechdancer.towerrepression.table
 
 import android.support.v4.app.Fragment
 import android.view.View
-import org.jetbrains.anko.AnkoComponent
-import org.jetbrains.anko.AnkoContext
-import org.jetbrains.anko.linearLayout
+import com.bin.david.form.core.SmartTable
+import com.bin.david.form.data.column.Column
+import com.bin.david.form.data.table.TableData
+import org.jetbrains.anko.*
+import org.jetbrains.anko.custom.ankoView
+import org.jetbrains.anko.custom.customView
 
 class TableUI : AnkoComponent<Fragment> {
+
     override fun createView(ui: AnkoContext<Fragment>): View = with(ui) {
         linearLayout {
-
+            customView<SmartTable<Unit>> {
+            }.lparams(matchParent, matchParent)
         }
     }
 }
